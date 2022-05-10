0875a806fb915ea21a86034e4b8454da7af31d42
André Ferreira <andre.ferreira.alves@gmail.com>
2022-05-10 12:02:55 -0300

    métodos get

--- bin/server_handle.dart
+++ bin/server_handle.dart
@@ -7,7 +7,17 @@ class ServerHandle {
   Handler get handler {
     final router = Router();
 
-    router.get('/', (Request request) => Response(200, body: 'primeira rota'));
+    // router.get('/', (Request request) => Response(200, body: 'primeira rota'));
+    router.get('/', (Request request) => Response.ok('primeira rota'));
+
+    router.get('/dados/<usuario>', (Request request, String usuario) => Response.ok('nome do usuário $usuario'));
+
+    // usando query parameter
+    router.get('/query', (Request request) {
+      String? nome = request.url.queryParameters['nome'];
+
+      return Response.ok('Info query: $nome');
+    });
 
     return router;
   }
