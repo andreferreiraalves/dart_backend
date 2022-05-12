4aa1e2b9eb49f7f0c4ec1bebc7639d59384c8850
André Ferreira <andre.ferreira.alves@gmail.com>
2022-05-11 22:24:43 -0300

    arquivo env

--- bin/infra/custom_server.dart
+++ bin/infra/custom_server.dart
@@ -2,12 +2,12 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 class CustomServer {
-  Future<void> initialize(Handler handler) async {
-    final String address = 'localhost';
-    final int port = 3000;
-
-    await shelf_io.serve(handler, address, port);
-
+  Future<void> initialize({
+    required Handler handler,
+    required String address,
+    required int port,
+  }) async {
     print('Server running http:\\$address:$port');
+    await shelf_io.serve(handler, address, port);
   }
 }
